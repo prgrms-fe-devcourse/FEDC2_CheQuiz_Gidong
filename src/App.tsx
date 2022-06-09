5a9b10cbb03cd3902835b6ceb6bb51e27ab2e240
padd60 <padd60@naver.com>
2022-06-09 23:56:29 +0900

    [feat, refactor] 레이아웃 기본 골조 구축(1440px 최대 크기 지정), 레이아웃 개념의 상위 라우팅 예제 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
 import Routers from '@/Routers';
 import reset from '@/styles/reset';
 import theme from '@/styles/theme';
 
+const Layout = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+`;
+
 function App(): JSX.Element {
   return (
-    <div>
+    <Layout>
       <ThemeProvider theme={theme}>
         <Global styles={reset} />
         <Routers />
       </ThemeProvider>
-    </div>
+    </Layout>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,37 @@
+import { Route } from 'react-router';
 import styled from '@emotion/styled';
-import { mobile } from '@/styles/theme';
+import Children from '@/pages/Children';
 
-// theme 주입 기능 작동 확인
-// 창 가로를 줄여 변화를 확인하세요.
-const Test = styled.div`
-  ${mobile} {
-    color: red;
-  }
+const HomeContainer = styled.div`
+  // 임시 보더
+  border: 1px solid;
+`;
+
+const HomeHeader = styled.header`
+  padding: 20px;
+  text-align: center;
+
+  // 임시 보더
+  border: 1px solid;
+`;
+
+const HomeMain = styled.main`
+  padding: 20px;
+  text-align: center;
+
+  // 임시 보더
+  border: 1px solid;
 `;
 
 function Home() {
   return (
-    <div>
-      메인 홈 화면입니다.
-      <Test>테스트입니다.</Test>
-    </div>
+    <HomeContainer>
+      <HomeHeader> 화면의 헤더 입니다.</HomeHeader>
+      <HomeMain>
+        메인 홈 화면의 바디입니다.
+        <Route exact path="/children" component={Children} />
+      </HomeMain>
+    </HomeContainer>
   );
 }
 
