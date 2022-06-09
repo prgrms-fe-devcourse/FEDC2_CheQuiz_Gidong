c4973e0873a573ba5fc0f3ab22b57a7e2ba3c4f1
Junhyuck Ko <mrbartrns@naver.com>
2022-06-09 23:26:40 +0900

    [feat , test] 컴포넌트와 mockDB연결 및 확인

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,12 +1,14 @@
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import Home from '@/pages/Home';
+import QuizSolve from '@/pages/QuizSolve';
 
 function Routers() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/solve" component={QuizSolve} />
         <Route path="*" render={() => <Redirect to="/" />} />
       </Switch>
     </BrowserRouter>
---
+++ src/pages/QuizSolve.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import QuizMockData from '@/assets/QuizMockData';
+
+function QuizSolve(): JSX.Element {
+  // TODO: 추후 api 연결 필요
+  const [state, setState] = useState(QuizMockData);
+
+  return (
+    <div>
+      <ul>
+        {state.map((quiz) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <li key={quiz._id}>
+            <ul>
+              <li>유저 정보: {quiz.author.fullName}</li>
+              <li>질문: {quiz.question}</li>
+              <li>설명: {quiz.des}</li>
+              <li>태그: {quiz.tag}</li>
+              <li>난이도: {quiz.difficulty}</li>
+              <li>중요도: {quiz.importance}</li>
+              <li>퀴즈 타입: {quiz.answerType}</li>
+              <li>정답: {quiz.answer}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default QuizSolve;
