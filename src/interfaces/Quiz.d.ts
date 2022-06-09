c0c75c3fd0ae094da17358080749e03bd986da9a
Junhyuck Ko <mrbartrns@naver.com>
2022-06-09 22:04:23 +0900

    [feat] Quiz content type 정의

---
+++ src/interfaces/Quiz.d.ts
@@ -0,0 +1,9 @@
+export interface QuizContent {
+  question: string;
+  des: string;
+  tag: string[];
+  difficulty: number;
+  importance: number;
+  answerType: 't/f' | 'numberType' | 'stringType';
+  answer: string;
+}
