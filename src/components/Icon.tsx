d9baff1e4a09d70d7e22926cf081fe060d685e0e
padd60 <padd60@naver.com>
2022-06-13 06:00:15 +0900

    [feat, etc] Icon 컴포넌트 추가 및 feature-icon 라이브러리 설치

---
+++ src/components/Icon.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import styled from '@emotion/styled';
+import { Buffer } from 'buffer';
+
+type propsType = {
+  name: string;
+  size: number;
+  strokeWidth: number;
+  color: string;
+  rotate: number;
+};
+
+const IconWrapper = styled.i`
+  display: inline-block;
+`;
+
+function Icon({
+  name,
+  size = 16,
+  strokeWidth = 2,
+  color = '#222',
+  rotate,
+  ...props
+}: propsType) {
+  const iconStyle = {
+    'stroke-width': strokeWidth,
+    stroke: color,
+    width: size,
+    height: size,
+  };
+  const shapeStyle = {
+    width: size,
+    height: size,
+    transform: rotate ? `rotate(${rotate}deg)` : undefined,
+  };
+  const icon = require('feather-icons').icons[name];
+  const svg = icon ? icon.toSvg(iconStyle) : '';
+  const base64 = Buffer.from(svg, 'utf8').toString('base64');
+  return (
+    <IconWrapper style={shapeStyle} {...props}>
+      <img alt={name} src={`data:image/svg+xml;base64,${base64}`} />
+    </IconWrapper>
+  );
+}
+
+export default Icon;
