c3bdc347b5712d6785fca77dbb678b398cb7fbe7
padd60 <padd60@naver.com>
2022-06-13 06:03:26 +0900

    [feat, style] 프로젝트 테마 스타일 정의 및 메서드 구현

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Theme } from '@emotion/react';
+
 /* eslint-disable no-shadow */
 const Size = {
   pc: '1200px', // 1200px
@@ -7,25 +10,89 @@ const Size = {
 };
 
 export const theme = {
-  colors: {
-    gray: '#707070',
-    brandColor: '#FF422E',
-    lightGray: '#B9B9B9',
-    lightDark: '#4A4646',
+  themeColors: {
+    pointColor: '#FCA311',
+    primary: '#14213D',
+    secondary: '#E5E5E5',
+  },
+  tagColor: {
+    green: '#ADE85A',
+    blue: '#0F9fff',
+    yellow: '#FFE100',
+    red: '#FF6254',
+    lightBrown: '#FED36F',
+    pink: '#FF937E',
+  },
+  textAndBackGroundColor: {
+    blackGray: '#212529',
+    DarkGray: '#343A40',
+    gray: '#495057',
+    lightGray: '#ADB5BD',
+    brightGray: '#CED4DA',
+    grayWhite: '#DEE2E6',
+    lightGrayWhite: '#E9ECEF',
+    white: '#F8F9FA',
+  },
+  borderStyle: {
+    borderWidth: '3px solid',
+    borderRadius: '8px',
   },
   media: {
     pc: `@media screen and (max-width: ${Size.pc})`,
     tab: `@media screen and (max-width: ${Size.tab})`,
     mobile: `@media screen and (max-width: ${Size.mobile})`,
   },
+  colors: '',
+  fontStyle: {
+    h1: 'font-size: 3rem; font-weight: 600',
+    h2: 'font-size: 2.5rem; font-weight: 600',
+    h3: 'font-size: 2rem; font-weight: 600',
+    large: 'font-size: 1.5rem; font-weight: 500',
+    medium: 'font-size: 1.25rem; font-weight: 500',
+    small: 'font-size: 1rem; font-weight: 500',
+    p: 'font-size: 0.9rem; font-weight: 400',
+    detail: 'font-size: 0.9rem; font-weight: 400',
+  },
 };
 
-export const pc = ({ theme }: any) => theme.media.pc;
-export const tab = ({ theme }: any) => theme.media.tab;
-export const mobile = ({ theme }: any) => theme.media.mobile;
-export const gray = ({ theme }: any) => theme.colors.gray;
-export const brandColor = ({ theme }: any) => theme.colors.brandColor;
-export const lightGray = ({ theme }: any) => theme.colors.lightGray;
-export const lightDark = ({ theme }: any) => theme.colors.lightDark;
+export const pc = (theme: Theme) => theme.media.pc;
+export const tab = (theme: Theme) => theme.media.tab;
+export const mobile = (theme: Theme) => theme.media.mobile;
+
+export const pointColor = (theme: Theme) => theme.themeColors.pointColor;
+export const primary = (theme: Theme) => theme.themeColors.primary;
+export const secondary = (theme: Theme) => theme.themeColors.secondary;
+export const tagBlue = (theme: Theme) => theme.tagColor.blue;
+export const tagGreen = (theme: Theme) => theme.tagColor.green;
+export const tagLightBrown = (theme: Theme) => theme.tagColor.lightBrown;
+export const tagPink = (theme: Theme) => theme.tagColor.pink;
+export const tagRed = (theme: Theme) => theme.tagColor.red;
+export const tagYellow = (theme: Theme) => theme.tagColor.yellow;
+
+export const DarkGray = (theme: Theme) => theme.textAndBackGroundColor.DarkGray;
+export const blackGray = (theme: Theme) =>
+  theme.textAndBackGroundColor.blackGray;
+export const brightGray = (theme: Theme) =>
+  theme.textAndBackGroundColor.brightGray;
+export const gray = (theme: Theme) => theme.textAndBackGroundColor.gray;
+export const grayWhite = (theme: Theme) =>
+  theme.textAndBackGroundColor.grayWhite;
+export const lightGray = (theme: Theme) =>
+  theme.textAndBackGroundColor.lightGray;
+export const lightGrayWhite = (theme: Theme) =>
+  theme.textAndBackGroundColor.lightGrayWhite;
+export const white = (theme: Theme) => theme.textAndBackGroundColor.white;
+
+export const borderRadius = (theme: Theme) => theme.borderStyle.borderRadius;
+export const borderWidth = (theme: Theme) => theme.borderStyle.borderWidth;
+
+export const h1 = (theme: Theme) => theme.fontStyle.h1;
+export const h2 = (theme: Theme) => theme.fontStyle.h2;
+export const h3 = (theme: Theme) => theme.fontStyle.h3;
+export const large = (theme: Theme) => theme.fontStyle.large;
+export const medium = (theme: Theme) => theme.fontStyle.medium;
+export const small = (theme: Theme) => theme.fontStyle.small;
+export const p = (theme: Theme) => theme.fontStyle.p;
+export const detail = (theme: Theme) => theme.fontStyle.detail;
 
 export default theme;
