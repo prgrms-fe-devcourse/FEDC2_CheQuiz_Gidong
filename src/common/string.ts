aef01bc3826357fd87569f5445e8880bafb5c3cd
padd60 <padd60@naver.com>
2022-06-13 06:04:18 +0900

    [feat, style] 태그 컴포넌트 구현 및 스타일 지정, 조건을 위한 고정 상수 추가

--- src/common/string.ts
+++ src/common/string.ts
@@ -1,3 +1,8 @@
 // 고정 스트링 상수들 정의
 
-export const TEST = 'test';
+export const GREEN = 'green';
+export const BLUE = 'blue';
+export const YELLOW = 'yellow';
+export const RED = 'red';
+export const PINK = 'pink';
+export const BROWN = 'brown';
