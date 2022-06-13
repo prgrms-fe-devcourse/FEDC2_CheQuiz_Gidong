c7a81b69a3c3790acc599d929d191bfa0269e1d9
Junhyuck Ko <mrbartrns@naver.com>
2022-06-13 23:01:19 +0900

    [feat] useInput hook 구현

---
+++ src/hooks/useInput/index.ts
@@ -0,0 +1,20 @@
+import React, { useCallback, useState } from 'react';
+
+type ReturnTypes<T> = [
+  T,
+  (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  React.Dispatch<React.SetStateAction<T>>,
+];
+
+function useInput<T = string>(initialValue: T): ReturnTypes<T> {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setValue(e.target.value as unknown as T);
+    },
+    [],
+  );
+  return [value, handler, setValue];
+}
+
+export default useInput;
