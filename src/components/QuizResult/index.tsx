2ba2279a4238c8e9e0e757b04e714f091e4a3865
Junhyuck Ko <mrbartrns@naver.com>
2022-06-14 16:00:23 +0900

    [feat] 컴포넌트 디자인 작업

--- src/components/QuizResult/index.tsx
+++ src/components/QuizResult/index.tsx
@@ -16,7 +16,8 @@ function QuizResult({ quiz, correct }: QuizResultProps) {
   );
   // TODO: useState에 들어갈 값 변경 필요 -> quiz likes 배열 참조할 것
   const [userLiked, setUserLiked] = useState(false);
-  const [commentOpened, setCommentOpened] = useState(false);
+  // TODO: useCollapse 만들기
+  const [containerOpened, setContainerOpened] = useState(false);
   const handleCommentSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // TODO: comment validation
@@ -25,43 +26,73 @@ function QuizResult({ quiz, correct }: QuizResultProps) {
   };
 
   return (
-    <Styled.StyledQuizResult>
-      <Styled.Title>
-        <Styled.TitleLeft>
-          <Styled.CorrectOrWrong correct={correct}>
+    <Styled.Box>
+      <Styled.Header>
+        <Styled.HeaderLeft>
+          <Styled.Sign reverse={false} color={correct ? 'blue' : 'red'}>
             {correct ? 'O' : 'X'}
-          </Styled.CorrectOrWrong>
-          <Styled.Sign>Q.</Styled.Sign>
-          <Styled.Question>{quiz.question}</Styled.Question>
-        </Styled.TitleLeft>
-        <Styled.TitleRight>
-          <button type="button">좋아요</button>
+          </Styled.Sign>
+          <Styled.Sign reverse={false} color="default">
+            Q.
+          </Styled.Sign>
+          <Styled.Text>{quiz.question}</Styled.Text>
+        </Styled.HeaderLeft>
+        <Styled.HeaderRight>
+          <button
+            type="button"
+            onClick={() => setContainerOpened((prev) => !prev)}
+          >
+            Toggle
+          </button>
+          <button type="button">좋아요 {quiz.likes.length}</button>
           <button type="button">댓글</button>
-        </Styled.TitleRight>
-      </Styled.Title>
-      <Styled.Container>
-        <div>A: {quiz.answer}</div>
-        <div>Description: {quiz.answerDescription}</div>
-        <div>좋아요 {quiz.likes.length}</div>
-        <form onSubmit={handleCommentSubmit}>
-          <h3>comment 작성하기</h3>
-          <input type="text" value={inputValue} onChange={handler} />
-          <button type="submit">댓글 쓰기</button>
-        </form>
+        </Styled.HeaderRight>
+      </Styled.Header>
+      <Styled.Container containerOpened={containerOpened}>
+        <Styled.Description>
+          <Styled.Sign reverse color={quiz.answer === 'true' ? 'blue' : 'red'}>
+            {quiz.answer === 'true' ? 'O' : 'X'}
+          </Styled.Sign>
+          <div>{quiz.answerDescription}</div>
+        </Styled.Description>
+        <Styled.Wrapper>
+          <form onSubmit={handleCommentSubmit}>
+            <h3>comment 작성하기</h3>
+            <Styled.Flex>
+              <Styled.ProfileImage>작성자 사진</Styled.ProfileImage>
+              <Styled.InputWrapper>
+                <Styled.Input
+                  type="text"
+                  value={inputValue}
+                  onChange={handler}
+                />
+              </Styled.InputWrapper>
+
+              {/** TODO: disabled when loading */}
+              <Styled.Button type="submit" color="point">
+                댓글 쓰기
+              </Styled.Button>
+            </Styled.Flex>
+          </form>
+        </Styled.Wrapper>
         <h1>comment 보기</h1>
         {comments.map((comment) => (
           <Styled.Comment key={comment._id}>
-            <Styled.CommentProfile>작성자 사진</Styled.CommentProfile>
+            <Styled.ProfileImage>작성자 사진</Styled.ProfileImage>
             <Styled.CommentCenter>
-              <div>작성자: {comment.author.fullName}</div>
-              <div>내용: {comment.comment}</div>
+              <div>
+                <Styled.Text>작성자: {comment.author.fullName}</Styled.Text>
+              </div>
+              <div>
+                <Styled.Text>내용: {comment.comment}</Styled.Text>
+              </div>
             </Styled.CommentCenter>
             <div>날짜: {comment.createdAt}</div>
             {/** TODO: 내가 작성한 댓글은 지울 수 있도록 로직 처리 */}
           </Styled.Comment>
         ))}
       </Styled.Container>
-    </Styled.StyledQuizResult>
+    </Styled.Box>
   );
 }
 
--- src/components/QuizResult/styles.tsx
+++ src/components/QuizResult/styles.tsx
@@ -1,35 +1,62 @@
 import styled from '@emotion/styled';
+import Link from 'react-router';
 
+// TODO: 임시 prop 바꾸기
+interface StyledButtonProps {
+  color?: 'point' | 'primary' | 'secondary';
+}
+
+interface StyledContainerProps {
+  containerOpened: boolean;
+}
+
+interface StyledSignProps {
+  reverse?: boolean;
+  color: 'blue' | 'red' | 'default';
+}
 export interface StyledQuizResultProps {
   correct: boolean;
 }
 
-export const StyledQuizResult = styled.div`
+export const Box = styled.div`
   border: 3px solid #14213d;
   border-radius: 0.5rem;
-  box-sizing: border-box;
+  * {
+    box-sizing: border-box;
+    font-family: 'MaplestoryOTFLight';
+  }
 `;
 
-export const Title = styled.div`
+export const Wrapper = styled(Box)`
+  margin: 1rem 0;
+  padding: 0.5rem;
+`;
+
+export const Container = styled.div<StyledContainerProps>`
+  margin: 0 1rem;
+  overflow: hidden;
+  transition: height 0.35s ease;
+`;
+
+export const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 72px;
+  height: 4.5rem;
   border-bottom: 3px solid #14213d;
-  box-sizing: border-box;
 `;
 
-export const TitleLeft = styled.div`
+export const HeaderLeft = styled.div`
   display: flex;
   align-items: center;
   gap: 1rem;
   padding: 1rem 1rem;
-  box-sizing: border-box;
 `;
 
-export const TitleRight = styled.div`
+export const HeaderRight = styled.div`
   display: flex;
   height: 100%;
+
   button {
     display: block;
     height: 100%;
@@ -38,6 +65,7 @@ export const TitleRight = styled.div`
     font-weight: bold;
     outline: none;
     cursor: pointer;
+
     :last-of-type {
       border-top-right-radius: 0.5rem;
     }
@@ -48,45 +76,105 @@ export const TitleRight = styled.div`
   }
 `;
 
-export const Sign = styled.div`
-  font-size: 2rem;
-  font-weight: bold;
-`;
-
-export const Question = styled.div`
-  line-height: 1.4;
-`;
-
-export const CorrectOrWrong = styled.div<StyledQuizResultProps>`
-  color: ${({ correct }) => (correct ? 'royalblue' : 'tomato')};
-  font-size: 2rem;
-  font-weight: bold;
-`;
-
-export const Container = styled.div`
-  margin: 0 1rem;
-`;
-
-export const Comment = styled.div`
-  display: flex;
-  margin: 1rem 0;
-  padding: 0.5rem;
-  height: 5rem;
-  border: 3px solid #14213d;
-  border-radius: 0.5rem;
-  line-height: 1.4;
-  font-family: 'Pretendard';
-`;
-
-export const CommentProfile = styled.div`
+export const Sign = styled.div<StyledSignProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-right: 0.5rem;
+  width: 46px;
+  height: 46px;
+  border: ${({ reverse }) => (reverse ? `1px solid #14213d` : 'none')};
+  border-radius: 0.5rem;
+  background-color: ${({ reverse, color }) => {
+    if (!reverse) return '#ffffff';
+    if (color === 'blue') return 'royalblue';
+    if (color === 'red') return 'tomato';
+    return '#14123d';
+  }};
+  color: ${({ reverse, color }) => {
+    if (reverse) return '#ffffff';
+    if (color === 'blue') return 'royalblue';
+    if (color === 'red') return 'tomato';
+    return '#14123d';
+  }};
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const Text = styled.span`
+  line-height: 1.4;
+`;
+
+export const Comment = styled(Wrapper)`
+  display: flex;
+  gap: 0.5rem;
+  margin: 1rem 0;
+`;
+
+export const ProfileImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
   background-color: rgba(0, 0, 0, 0.2);
   font-family: 'MaplestoryOTFLight';
 `;
 
 export const CommentCenter = styled.div`
   flex: 1;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const TextInput = styled.input`
+  flex: 1;
+  display: block;
+  padding: 0.5rem;
+  border: 3px solid #14213d;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  outline: none;
+`;
+
+export const InputWrapper = styled(Wrapper)`
+  flex: 1;
+  margin: 0;
+  :hover {
+    border-color: #565656;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  border: none;
+  font-size: 1rem;
+  outline: none;
+`;
+
+export const Button = styled.button<StyledButtonProps>`
+  padding: 0.5rem;
+  border: 3px solid #14213d;
+  border-radius: 0.5rem;
+  background-color: ${({ color }) => {
+    if (color === 'point') return '#fca211';
+    if (color === 'primary') return '#14213d';
+    return '#e5e5e5';
+  }};
+  outline: none;
+  cursor: pointer;
+  :hover {
+    // TODO: 각 color 마다 추가 작업 필요
+    background-color: #fca211d9;
+  }
 `;
