006914310ea0d330747532320e9bec62213149a9
chmini <beelzebule@naver.com>
2022-06-14 16:57:44 +0900

    [feat] InputBox 컴포넌트 추가

---
+++ src/components/Form/InputBox/index.tsx
@@ -0,0 +1,25 @@
+import { useField } from 'formik';
+import * as S from './styles';
+
+interface Props {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
+}
+
+function InputBox({ label, ...props }: Props) {
+  const [field, meta] = useField(props);
+
+  return (
+    <S.InputBox>
+      <S.Label htmlFor={props.name}>{label}</S.Label>
+      <S.Input {...props} {...field} />
+      {meta.touched && meta.error ? (
+        <S.ErrorText>{meta.error}</S.ErrorText>
+      ) : null}
+    </S.InputBox>
+  );
+}
+
+export default InputBox;
---
+++ src/components/Form/InputBox/styles.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 8px 0;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 4px;
+`;
+
+// TODO: 전역 스타일 컬러 적용
+export const Input = styled.input`
+  width: 100%;
+  border: 2px solid #14213d;
+  border-radius: 8px;
+  box-sizing: border-box;
+  outline: none;
+  padding: 8px;
+  font-size: 1rem;
+`;
+
+export const ErrorText = styled.div`
+  color: #ff0000;
+  font-size: 0.9rem;
+`;
