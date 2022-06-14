61a418f16c5c3fa5fb9a026533f368635599ff8b
smilehae <mihaepyun@gmail.com>
2022-06-15 03:00:42 +0900

    [feat] 좋아요, 댓글 관련 뱃지 구현, 뱃지 시스템 분리 완료

---
+++ src/components/UserInfo/UserInfoCard/breakpoints.ts
@@ -0,0 +1,67 @@
+export const levelBreakpoints = [
+  { level: 0, color: '#977C37' },
+  { level: 10, color: '#5f7161' },
+  { level: 50, color: '#6D8B74' },
+  { level: 100, color: '#00FFAB' },
+  { level: 500, color: '#0D99FF' },
+  { level: 1000, color: '#FFBEB8' },
+  { level: 5000, color: '#FAFF00' },
+  { level: 10000, color: '#F30E5C' },
+  { level: 50000, color: '#FF1809' },
+];
+
+export const imageBreakpoints = [
+  { level: 10, imageId: '100120' },
+  { level: 50, imageId: '100121' },
+  { level: 100, imageId: '100122' },
+  { level: 500, imageId: '100123' },
+  { level: 1000, imageId: '100124' },
+  { level: 5000, imageId: '2510000' },
+  { level: 10000, imageId: '8600006' },
+  { level: 50000, imageId: '6400006' },
+];
+
+export const commentBreakpoints = [
+  {
+    count: 0,
+    color: '#8E05C2',
+    text: '무뚝뚝그자체',
+    exact: true,
+  },
+  {
+    count: 10,
+    color: '#ADE85A',
+    text: '투머치토커',
+    exact: false,
+  },
+];
+
+export const likeBreakpoints = [
+  {
+    count: 0,
+    color: '#7E7474',
+    text: '묵언수행중',
+    exact: true,
+  },
+  {
+    count: 10,
+    color: '#FF937E ',
+    text: '사랑꾼',
+    exact: false,
+  },
+];
+
+export const likeAndCommentBreakpoints = [
+  {
+    count: 0,
+    color: '#999',
+    text: '혼자가좋아',
+    exact: true,
+  },
+  {
+    count: 10,
+    color: '#FF6254',
+    text: '소통왕',
+    exact: false,
+  },
+];
--- src/components/UserInfo/UserInfoCard/index.tsx
+++ src/components/UserInfo/UserInfoCard/index.tsx
@@ -21,6 +21,7 @@ import {
   userQuizMockList as userQuizList,
   UserInfoMockData as userMockData,
 } from '@/assets/UserInfoMockData';
+import * as Breakpoints from './breakpoints';
 
 interface userProps {
   id: string;
@@ -40,29 +41,6 @@ function UserInfoCard({ id }: userProps) {
   const currentExp = userData.totalExp - (level - 1) * MAXEXP;
   const expPercent = Math.floor((currentExp / MAXEXP) * 100);
 
-  const levelBreakpoints = [
-    { level: 0, color: '#977C37' },
-    { level: 10, color: '#5f7161' },
-    { level: 50, color: '#6D8B74' },
-    { level: 100, color: '#00FFAB' },
-    { level: 500, color: '#0D99FF' },
-    { level: 1000, color: '#FFBEB8' },
-    { level: 5000, color: '#FAFF00' },
-    { level: 10000, color: '#F30E5C' },
-    { level: 50000, color: '#FF1809' },
-  ];
-
-  const imageBreakpoints = [
-    { level: 10, imageId: '100120' },
-    { level: 50, imageId: '100121' },
-    { level: 100, imageId: '100122' },
-    { level: 500, imageId: '100123' },
-    { level: 1000, imageId: '100124' },
-    { level: 5000, imageId: '2510000' },
-    { level: 10000, imageId: '8600006' },
-    { level: 50000, imageId: '6400006' },
-  ];
-
   // TODO: 유저리스트 비동기 처리
   const getRank = useCallback(() => {
     const sortedUserList = userList
@@ -80,7 +58,7 @@ function UserInfoCard({ id }: userProps) {
 
   const getImage = () => {
     let imageId = '100120';
-    imageBreakpoints.forEach((breakpoint) => {
+    Breakpoints.imageBreakpoints.forEach((breakpoint) => {
       if (level >= breakpoint.level) {
         imageId = breakpoint.imageId;
       }
@@ -110,30 +88,84 @@ function UserInfoCard({ id }: userProps) {
 
     // 레벨 별 뱃지
     let levelBadgeId = 0;
-    levelBreakpoints.forEach((badge, index) => {
+    Breakpoints.levelBreakpoints.forEach((badge, index) => {
       if (level > badge.level) {
         levelBadgeId = index;
       }
     });
 
     badges.push({
-      id: `badgeLv${levelBadgeId}`,
-      color: levelBreakpoints[levelBadgeId].color,
+      id: `badgeLevel${levelBadgeId}`,
+      color: Breakpoints.levelBreakpoints[levelBadgeId].color,
       content: `${
         levelBadgeId !== 0
-          ? `내가 레벨 ${levelBreakpoints[levelBadgeId].level} 이라니!`
+          ? `내가 레벨 ${Breakpoints.levelBreakpoints[levelBadgeId].level} 이라니!`
           : `뉴비`
       }`,
     });
     // TODO: 퀴즈 별 뱃지 => typescript에 익숙하지 않아 추후 구현 예정..
 
     // 좋아요, 댓글 당 뱃지
-    console.log(userData.comments.length, userData.likes.length);
 
-    // 댓글 10개 이상 & 좋아요 10개 이상 소통왕.. 댓글 10개 이상 투머치토커..
-    // 좋아요 10개 이상 사랑꾼, 좋아요 없음 무뚝뚝그자체, 댓글 없음 묵언수행중,
-    // 댓글 없음 & 좋아요 없음 혼자가좋아
+    Breakpoints.commentBreakpoints.forEach((breakpoint, index) => {
+      if (breakpoint.exact) {
+        if (userData.comments.length === breakpoint.count) {
+          badges.push({
+            id: `badgeComment${index}`,
+            color: breakpoint.color,
+            content: breakpoint.text,
+          });
+        }
+      } else if (userData.comments.length >= breakpoint.count) {
+        badges.push({
+          id: `badgeComment${index}`,
+          color: breakpoint.color,
+          content: breakpoint.text,
+        });
+      }
+    });
 
+    Breakpoints.likeBreakpoints.forEach((breakpoint, index) => {
+      if (breakpoint.exact) {
+        if (userData.likes.length === breakpoint.count) {
+          badges.push({
+            id: `badgeLike${index}`,
+            color: breakpoint.color,
+            content: breakpoint.text,
+          });
+        }
+      } else if (userData.likes.length >= breakpoint.count) {
+        badges.push({
+          id: `badgeLike${index}`,
+          color: breakpoint.color,
+          content: breakpoint.text,
+        });
+      }
+    });
+
+    Breakpoints.likeAndCommentBreakpoints.forEach((breakpoint, index) => {
+      if (breakpoint.exact) {
+        if (
+          userData.likes.length === breakpoint.count &&
+          userData.comments.length === breakpoint.count
+        ) {
+          badges.push({
+            id: `badgeLikeComment${index}`,
+            color: breakpoint.color,
+            content: breakpoint.text,
+          });
+        }
+      } else if (
+        userData.likes.length >= breakpoint.count &&
+        userData.comments.length >= breakpoint.count
+      ) {
+        badges.push({
+          id: `badgeLikeComment${index}`,
+          color: breakpoint.color,
+          content: breakpoint.text,
+        });
+      }
+    });
     return badges;
   };
   getBadges();
