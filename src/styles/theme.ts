e4bb22789114ae10196713effe74ea1dceb91fe7
padd60 <padd60@naver.com>
2022-06-16 00:19:56 +0900

    [refactor] 오류 수정6 emotion

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -63,7 +63,7 @@ export const theme = {
   },
 };
 
-// 글로벌 emotion props 타입 지정 코드
+// 글로벌 emotion props 타입 지정
 
 export const pc = (props: any) => props.theme.media.pc;
 export const tab = (props: any) => props.theme.media.tab;
