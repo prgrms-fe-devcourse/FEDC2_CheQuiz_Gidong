f410914562e342c28c13a61dd15525a687866c9a
padd60 <padd60@naver.com>
2022-06-17 01:25:42 +0900

    [fix] apiCall 함수 제거에 따른 api 관련 함수 코드 변경

--- src/api/getRankAll.ts
+++ src/api/getRankAll.ts
@@ -1,13 +1,13 @@
+import apiInstance from '@/api/apiInstance';
 import { UserAPI } from '@/interfaces/UserAPI';
-import ApiCall from '@/utils/apiCall';
 import { rankingUrl } from '@/utils/apiUrl';
 
 const getRankAll = async () => {
   try {
-    const { data }: { data: UserAPI[] } = await ApiCall(
-      'get',
-      rankingUrl.getAllUsers,
-    );
+    const { data }: { data: UserAPI[] } = await apiInstance({
+      method: 'get',
+      url: rankingUrl.getAllUsers,
+    });
     return data;
   } catch (error) {
     console.log(error);
