6ec998dc4c3e6247f279d8e83143fb65bae2b99c
smilehae <mihaepyun@gmail.com>
2022-06-17 01:49:58 +0900

    [feat] 유저 인터페이스 + 디폴트 값 추가

---
+++ src/assets/UserInfoDefault.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_USER_DATA = {
+  id: '62aaed65e193b3692eddfa8f',
+  posts: [],
+  likes: [],
+  comments: [],
+  following: [],
+  fullName: '프롱이',
+  email: 'asdf@asdf.com',
+  totalExp: 0,
+};
