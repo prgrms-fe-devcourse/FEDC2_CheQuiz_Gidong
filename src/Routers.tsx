15def22b29d2c6e2841f47bfa5a3603abf9966fd
chmini <beelzebule@naver.com>
2022-06-19 16:14:37 +0900

    [refactor] AnotherLayout 컴포넌트 삭제

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,14 +1,13 @@
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
+
 import Home from '@/pages/Home';
 import QuizCreate from '@/pages/QuizCreate';
 import QuizResultPage from '@/pages/QuizResultPage';
 import QuizSolvePage from '@/pages/QuizSolvePage';
-import AnotherLayout from '@/pages/AnotherLayout';
 import Error from '@/pages/Error';
 import UserInfo from '@/pages/UserInfo';
 
-// 라우터 추가 시 path가 '/'인 메인페이지는 제일 하단에 위치시켜주세요.
 function Routers() {
   return (
     <BrowserRouter>
@@ -17,7 +16,6 @@ function Routers() {
         <Route exact path="/result" component={QuizResultPage} />
         <Route path="/user" component={UserInfo} />
         <Route exact path="/solve" component={QuizSolvePage} />
-        <Route path="/another" component={AnotherLayout} />
         <Route path="/error" component={Error} />
         <Route path="/" component={Home} />
         <Route path="*" render={() => <Redirect to="/" />} />
