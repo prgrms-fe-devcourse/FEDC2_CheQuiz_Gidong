d1bff15b246a7b87a7412360d2897cc845b1cb6b
smilehae <mihaepyun@gmail.com>
2022-06-19 15:55:01 +0900

    [feat] 탭 선택에 따라 다른 문제 보여주기 구현

--- src/components/UserInfo/UserInfoTab/index.tsx
+++ src/components/UserInfo/UserInfoTab/index.tsx
@@ -4,11 +4,31 @@ import * as S from './styles';
 import { PostAPIUserInfo } from '@/interfaces/PostAPI';
 import { UserQuizType } from '@/interfaces/UserAPI';
 import UserQuizItem from '../UserQuizItem';
+import TabItem from '../UserInfoTabItem';
 
 function UserInfoTab({ id }: { id: string }) {
   const [madeQuizzes, setMadeQuizzes] = useState([]);
   const [commentedQuizzes, setCommentedQuizzes] = useState([]);
   const [likedQuizzes, setLikedQuizzes] = useState([]);
+
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const tabMapper = [madeQuizzes, commentedQuizzes, likedQuizzes];
+
+  const tabs = [
+    {
+      id: 0,
+      title: '만든 문제',
+    },
+    {
+      id: 1,
+      title: '댓글 단 문제',
+    },
+    {
+      id: 2,
+      title: '좋아요 한 문제',
+    },
+  ];
 
   useEffect(() => {
     const updateAllQuiz = async () => {
@@ -44,18 +64,28 @@ function UserInfoTab({ id }: { id: string }) {
       setMadeQuizzes(userMadeQuizzes);
       setCommentedQuizzes(userCommentQuizzes);
       setLikedQuizzes(userLikesQuizzes);
-      console.log(userMadeQuizzes);
     };
 
     updateAllQuiz();
   }, [id]);
+
+  const updateClickedTab = (tabId: number) => {
+    setCurrentTab(tabId);
+  };
   return (
     <S.TabWrapper>
       <S.TabMenus>
         <S.TabItemContainer>
-          <S.TabItem selected>만든 문제</S.TabItem>
-          <S.TabItem>댓글 단 문제</S.TabItem>
-          <S.TabItem>좋아요 한 문제</S.TabItem>
+          {tabs.map((tab) => (
+            <TabItem
+              key={tab.id}
+              title={tab.title}
+              selected={tab.id === currentTab}
+              handleClick={() => {
+                updateClickedTab(tab.id);
+              }}
+            />
+          ))}
         </S.TabItemContainer>
         <S.ButtonWrapper>
           <S.Button color="#5B9785">퀴즈 수정</S.Button>
@@ -65,7 +95,7 @@ function UserInfoTab({ id }: { id: string }) {
 
       <S.TabContent>
         <S.UserQuizContainer>
-          {madeQuizzes.map((quiz: UserQuizType, index) => (
+          {tabMapper[currentTab].map((quiz: UserQuizType) => (
             <UserQuizItem
               key={quiz.id}
               question={quiz.question}
@@ -73,52 +103,6 @@ function UserInfoTab({ id }: { id: string }) {
               commentCount={quiz.comments.length}
             />
           ))}
-          {/* TODO: 데이터가 충분히 많아지면 지우기 ( 스크롤 테스트용) */}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />{' '}
-          <UserQuizItem
-            question="자바스크립트는 싱글스레드로 동작한다"
-            likeCount={3}
-            commentCount={122}
-          />
         </S.UserQuizContainer>
       </S.TabContent>
     </S.TabWrapper>
--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header';
 import UserInfoCard from '@/components/UserInfo/UserInfoCard';
+import UserInfoTab from '@/components/UserInfo/UserInfoTab';
 
 function UserInfo() {
   return (
     <div>
       <Header />
       <UserInfoCard id="62aafe7ce193b3692eddfc77" />
+      <UserInfoTab id="62aafe7ce193b3692eddfc77" />
     </div>
   );
 }
