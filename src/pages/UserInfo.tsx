c4df6cfe32aadfbac1b0914e7af24c77c3093230
smilehae <mihaepyun@gmail.com>
2022-06-20 17:16:29 +0900

    [feat] valid id인지 확인하는 로직 추가

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -3,17 +3,30 @@ import { useParams } from 'react-router';
 import Header from '@/components/Header';
 import UserInfoCard from '@/components/UserInfo/UserInfoCard';
 import UserInfoTab from '@/components/UserInfo/UserInfoTab';
+import { fetchUserList } from '@/api/user';
+import { UserAPI } from '@/interfaces/UserAPI';
 
 interface Props {
   userId: string;
 }
 function UserInfo() {
+  const [valid, setValid] = useState(false);
   const [id, setId] = useState('');
 
   const params = useParams<Props>();
   useEffect(() => {
+    const setValidId = async (urlId: string) => {
+      const apiData = await fetchUserList();
+      const idList = apiData.map((user: UserAPI) => user._id);
+      const isValid = idList.some((item: string) => item === urlId);
+      setValid(isValid);
+      if (isValid) {
+        setId(urlId);
+      }
+    };
+
     const { userId } = params;
-    setId(userId);
+    setValidId(userId);
   }, [params]);
 
   return (
