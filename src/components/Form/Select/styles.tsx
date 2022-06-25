be7f681fb0dd13cee5698c75692be7a75c1bfe5c
Insoo-Seo <kocon135@gmail.com>
2022-06-25 15:01:18 +0900

    [fix] RandomQuiz 카테고리 선택 input ellipsis 속성 추가

--- src/components/Form/Select/styles.tsx
+++ src/components/Form/Select/styles.tsx
@@ -8,10 +8,13 @@ export const SelectBox = styled.select`
   padding: 0.25rem 2rem;
   font-family: 'MaplestoryOTFLight';
   ${small};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
   border: 3px solid ${DarkGray};
   border-radius: 0.5rem;
   box-sizing: border-box;
-
   appearance: none;
   outline: none;
   background: url(${arrowIcon}) 95.5% center/10% no-repeat;
