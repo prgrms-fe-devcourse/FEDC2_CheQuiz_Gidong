c44624e9a1d869154515e806b070c626689f581d
padd60 <padd60@naver.com>
2022-07-19 01:06:59 +0900

    [feat] border 스타일 파운데이션 추가

--- src/foundations/border/index.ts
+++ src/foundations/border/index.ts
@@ -1,2 +1,4 @@
-export const BORDER_STYLE = '0.1875rem solid';
+export const BORDER_LINE_STYLE = '0.1875rem solid';
+export const BORDER_DASH_STYLE = '0.1875rem dashed';
+export const BORDER_DOT_STYLE = '0.1875rem dotted';
 export const BORDER_RADIUS_STYLE = '0.5rem';
