78a49beaf88cd7fc963b1b8a128bfda054ee95f8
Junhyuck Ko <mrbartrns@naver.com>
2022-09-14 17:10:32 +0900

    [refactor] 사용되지 않는 import 제거

--- src/hooks/useQuiz/useQuiz.ts
+++ src/hooks/useQuiz/useQuiz.ts
@@ -3,7 +3,6 @@ import { useCallback, useState } from 'react';
 import QuizServices from './useQuiz.helper';
 
 import type { QuizType } from './useQuiz.helper';
-import type { Quiz as QuizInterface } from '@/interfaces/Quiz';
 
 type ReturnType = [
   QuizType[],
@@ -11,7 +10,6 @@ type ReturnType = [
   (channelId: string) => Promise<void>
 ];
 
-// ANCHOR: QuizResultPage 컨텐츠는 퀴즈보다는 Post의 성격이 강해서, 다른 hook에 넣을 예정
 const useQuiz = (): ReturnType => {
   const [quizzes, setQuizzes] = useState<QuizType[]>([]);
 
