eb19f8335e78bf529c71f6ff2c4753fbb85ece5c
Insoo-Seo <kocon135@gmail.com>
2022-10-28 22:48:20 +0900

    [feat] react-modal 패키지 설치
    (cherry picked from commit 03c7347c371a9ec1dc93600f8ce333b864b5995d)

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client';
+import ReactModal from 'react-modal';
 
 import App from '@/App';
+
+import ModalsProvider from './components/shared/Modal/ModalsProvider';
+
+ReactModal.setAppElement('#root');
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <ModalsProvider>
+    <App />
+  </ModalsProvider>
+);
