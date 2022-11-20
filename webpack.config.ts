bba626e383eb82d7be8952d6620d5f93185c9847
Junhyuck Ko <mrbartrns@naver.com>
2022-11-20 11:29:10 +0900

    [etc] argv를 사용하도록 변경 및 개발 환경 모드에서 비동기적으로 타입 체킹을 진행행

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,53 +11,69 @@ interface Configuration extends WebpackConfiguration {
   devserver?: WebpackDevServerConfiguration;
 }
 
-const config: Configuration = {
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      // ANCHOR: '@' 경로는 삭제 예정이므로, import시 하위 폴더 alias로 import 변경
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-    },
-  },
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-    new DotenvPlugin({
-      systemvars: true,
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
+type WebpackArguments = {
+  [key: string]: string;
 };
 
-export default config;
+export default (env: NodeJS.ProcessEnv, argv: WebpackArguments) => {
+  const isProdcution = argv.mode === 'production';
+  const isDevelopment = argv.mode === 'development';
+
+  const config: Configuration = {
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        // ANCHOR: '@' 경로는 삭제 예정이므로, import시 하위 폴더 alias로 import 변경
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        async: isDevelopment,
+        typescript: {
+          diagnosticOptions: {
+            syntactic: true,
+          },
+        },
+      }),
+      new DotenvPlugin({
+        systemvars: true,
+      }),
+    ],
+    devServer: {
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
+
+  return config;
+};
